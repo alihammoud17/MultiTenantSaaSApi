829703d8213ed8fb45c7165586b60f79823ec55b
ahammoud <ahammoud@PORT2024090201>
2026-03-13 09:33:34 +0200

    Add: Package Microsoft.AspNetCore.TestHost

--- Tests/Integration/ApiWebApplicationFactory.cs
+++ Tests/Integration/ApiWebApplicationFactory.cs
@@ -4,6 +4,7 @@ using Domain.Outputs;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
